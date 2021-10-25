6eb3b33d5c07eaab7d6948e7864365e39b1a6b4e
ggnid <popyer001@gmail.com>
2021-10-25 18:17:06 +0800

    Updates

---
+++ precache-manifest.f7d10297f62be9b08cd220c0966696e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3653fba2b9facbefc1945e4cc40bef20",
+    "url": "/zinft/index.html"
+  },
+  {
+    "revision": "f9a494bdff9b6e0ca991",
+    "url": "/zinft/static/css/main.323a554d.chunk.css"
+  },
+  {
+    "revision": "9d211fa8a440c66ca58e",
+    "url": "/zinft/static/js/2.75332276.chunk.js"
+  },
+  {
+    "revision": "f9a494bdff9b6e0ca991",
+    "url": "/zinft/static/js/main.67018700.chunk.js"
+  },
+  {
+    "revision": "c2cb47bd34bede3d2b00",
+    "url": "/zinft/static/js/runtime-main.2ec6d8cc.js"
+  },
+  {
+    "revision": "8cea1391b8e0717ea6532ebfa5cf4a34",
+    "url": "/zinft/static/media/twitter-logo.8cea1391.svg"
+  }
+]);
